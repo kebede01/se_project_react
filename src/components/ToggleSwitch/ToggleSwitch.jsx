930714b184ac8629d9ce9c9579df3a5401ae3bc9
Kebede <johnzeeze@yahoo.com>
2025-06-28 18:19:50 -0700

    feat:toggleSwitch done with pure css

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -9,13 +9,8 @@ const ToggleSwitch = () => {
        <label htmlFor="toggle" className="toggle-switch__label">
          <input type="checkbox" id="toggle" name="toggle" className="toggle-switch__checkbox" onChange={ handleToggleSwitchChange} />
          <span className='toggle__switch-circle'></span>
-         <span className={`toggle__switch-text toggle__switch-text_F ${currentTemperatureUnit === "F"
-           ? 'toggle__switch-text_color-white'
-           : ""
-         }`}>F</span>
-         <span className={`toggle__switch-text toggle__switch-text_C ${currentTemperatureUnit === "C"
-           ? 'toggle__switch-text_color-white'
-           : ""}`}>C</span>
+         <span className='toggle__switch-text toggle__switch-text_F'>F</span>
+         <span className='toggle__switch-text toggle__switch-text_C '>C</span>
         
        </label>
      </>
