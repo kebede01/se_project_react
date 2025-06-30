a4cc06e4462b72b95fe6d5dc0322dc9a75252d9f
Kebede <johnzeeze@yahoo.com>
2025-06-29 17:09:05 -0700

    feat:Delete card api handled

---
+++ src/components/Delete/Delete.jsx
@@ -0,0 +1,31 @@
+import './Delete.css'
+
+function DeleteModal({ activeModal, onCloseModal, onDeleteModalDeletButton, selectedCard }) {
+   
+  function handleCallBack () {
+
+  onDeleteModalDeletButton(selectedCard);
+  }
+
+         
+      return (
+         <div className={`modal modal__card ${activeModal === "delete" ? "modal_opened" : ""}`} >
+            <div className="modal__content modal__content_type-delete">
+               <button type="button" className="modal__close modal__close-btn_type-preview" onClick={onCloseModal}>
+               </button>
+        
+               <h2 className="modal__caption">Are you sure you want to delete this item?
+                  This action is irreversible.
+               </h2>
+               <button type="button" className='modal__button' onClick={handleCallBack}>Yes delete item</button>
+        
+               <button type="button" onClick={onCloseModal} className='modal__button'>Cancel</button>
+         
+        
+          
+            </div>
+         </div>
+    
+      )
+   }
+export default DeleteModal;
